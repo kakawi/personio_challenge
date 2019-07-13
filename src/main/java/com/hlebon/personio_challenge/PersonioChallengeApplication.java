5765fd1b62fa8a74b4a65f2e3f0802aa0d06dfed
Hleb Bandarenka <hleb.bandarenka@gmail.com>
2019-07-13 09:42:13 +0200

    initial commit

---
+++ src/main/java/com/hlebon/personio_challenge/PersonioChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.hlebon.personio_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonioChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonioChallengeApplication.class, args);
+    }
+
+}
